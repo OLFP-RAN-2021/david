d3e725fab2b30b12cb4cf61bad0e2b3e4dfbb8d6
Plusdepseudodispo <commande@jagware.org>
2021-04-01 14:39:25 +0200

    Regex pour controle de l'email

--- last_CV/assets/js/CV.js
+++ last_CV/assets/js/CV.js
@@ -1,5 +1,3 @@
-
-
 let name_field = document.getElementById("name");
 let mail_field = document.getElementById("mail");
 let msg_field = document.getElementById("msg");
@@ -13,9 +11,19 @@ let value_name_field=name_field.value;
 let value_mail_field=mail_field.value;
 let value_msg_field=msg_field.value;
 
+var emailReg = new RegExp(/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/);
+let valid_email_flag=emailReg.test(value_mail_field)==false;
+if (valid_email_flag ) 
+        document.getElementById("mail").style.color = "red";
+else
+        document.getElementById("mail").style.color = "white";   
+
+
 console.log("Debut");
 console.log(value_name_field);
 console.log(value_mail_field);
 console.log(value_msg_field);
 console.log("end");
+
+
 }
