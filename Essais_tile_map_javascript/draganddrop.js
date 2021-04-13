b242d84b4e2a85d0897c80c554bafc347692d4bf
Plusdepseudodispo <commande@jagware.org>
2021-04-13 11:06:56 +0200

    Rotate avec size qui fonctionne

--- Essais_tile_map_javascript/draganddrop.js
+++ Essais_tile_map_javascript/draganddrop.js
@@ -1,11 +1,18 @@
 let Angle = 0;
-let cnv_w = 300;
-let cnv_h = 200;
+let cnv_w = 0;
+let cnv_h = 0;
 let canevas = document.getElementById('Canevas');
 let context = canevas.getContext('2d');
 
 let left_button = document.getElementById("left");
 let right_button = document.getElementById("right");
+
+console.log("coucou !");
+
+// Plus 2 pour les bordures
+
+Console_les_size()
+
 
 left_button.addEventListener("click", left_rotate);
 right_button.addEventListener("click", right_rotate);
@@ -63,9 +70,8 @@ function Read_and_display_image(file) {
 
     reader.addEventListener('load', (event) => {
         context.drawImage(img_prov, 0, 0, cnv_w, cnv_h);
-        // cnv_w = canevas.clientHeight;
-        // cnv_h = canevas.clientWidth;
-        // console.log(cnv_w, cnv_h);
+
+        Console_les_size()
     });
 
 }
@@ -78,4 +84,15 @@ function Do_the_rotation_yahoooo(degrees) {
     context.rotate(degrees * Math.PI / 180);
     context.drawImage(img_prov, -(cnv_w / 2), -(cnv_h / 2), cnv_w, cnv_h);
     context.restore();
+
+    Console_les_size()
+
+}
+
+function Console_les_size()
+{
+    cnv_h = canevas.clientHeight + 2;
+    cnv_w = canevas.clientWidth + 2;
+    console.log(cnv_w, cnv_h);
+   
 }
